50f7f0b60f457b2157b23288234a9b3fe5d7e290
Aung Myint Myat <61227624+AungMyintMyat-30@users.noreply.github.com>
2025-04-27 00:42:29 +0630

    Update ProductsControllerTests.cs

--- ProductAPI.Test/ProductsControllerTests.cs
+++ ProductAPI.Test/ProductsControllerTests.cs
@@ -29,12 +29,10 @@ namespace ProductAPI.Test
         [Fact]
         public async Task GetAll_ReturnsOkResult_WithProducts()
         {
-            // Arrange
             var products = new List<Product> { new Product { Id = 1, StockName = "TestProduct" } };
             var paginationResult = new PaginationResultModel<List<Product>>(1, products);
             _mockService.Setup(s => s.GetAllAsync(0, 10)).ReturnsAsync(paginationResult);
 
-            // Act
             var result = await _controller.GetAll(0, 10);
 
             // Assert
@@ -45,11 +43,9 @@ namespace ProductAPI.Test
         [Fact]
         public async Task GetById_ReturnsOkResult_WhenProductExists()
         {
-            // Arrange
             var product = new Product { Id = 1, StockName = "Product1" };
             _mockService.Setup(s => s.GetByIdAsync(1)).ReturnsAsync(product);
 
-            // Act
             var result = await _controller.GetById(1);
 
             // Assert
@@ -60,10 +56,8 @@ namespace ProductAPI.Test
         [Fact]
         public async Task GetById_ReturnsNotFound_WhenProductDoesNotExist()
         {
-            // Arrange
             _mockService.Setup(s => s.GetByIdAsync(1)).ReturnsAsync((Product?)null);
 
-            // Act
             var result = await _controller.GetById(1);
 
             // Assert
@@ -74,27 +68,23 @@ namespace ProductAPI.Test
         [Fact]
         public async Task Create_ReturnsCreatedResult_WhenProductIsCreated()
         {
-            // Arrange
             var product = new Product { Id = 1, StockName = "NewProduct" };
             _mockService.Setup(s => s.GetByIdAsync(product.Id)).ReturnsAsync((Product?)null);
             _mockService.Setup(s => s.CreateAsync(product)).ReturnsAsync(product);
 
-            // Act
             var result = await _controller.Create(product);
 
             // Assert
-            var createdResult = Assert.IsType<CreatedResult>(result); // <-- Correct type here
+            var createdResult = Assert.IsType<CreatedResult>(result);
             Assert.Equal(201, createdResult.StatusCode);
         }
 
         [Fact]
         public async Task Create_ReturnsBadRequest_WhenProductExists()
         {
-            // Arrange
             var product = new Product { Id = 1, StockName = "DuplicateProduct" };
             _mockService.Setup(s => s.GetByIdAsync(product.Id)).ReturnsAsync(product);
 
-            // Act
             var result = await _controller.Create(product);
 
             // Assert
@@ -105,11 +95,9 @@ namespace ProductAPI.Test
         [Fact]
         public async Task Update_ReturnsOkResult_WhenProductUpdated()
         {
-            // Arrange
             var product = new Product { Id = 1, StockName = "UpdatedProduct" };
             _mockService.Setup(s => s.UpdateAsync(product)).ReturnsAsync(true);
 
-            // Act
             var result = await _controller.Update(1, product);
 
             // Assert
@@ -120,10 +108,8 @@ namespace ProductAPI.Test
         [Fact]
         public async Task Update_ReturnsBadRequest_WhenIdMismatch()
         {
-            // Arrange
             var product = new Product { Id = 2, StockName = "MismatchProduct" };
 
-            // Act
             var result = await _controller.Update(1, product);
 
             // Assert
@@ -134,11 +120,9 @@ namespace ProductAPI.Test
         [Fact]
         public async Task Update_ReturnsNotFound_WhenProductNotFound()
         {
-            // Arrange
             var product = new Product { Id = 1, StockName = "NonExistingProduct" };
             _mockService.Setup(s => s.UpdateAsync(product)).ReturnsAsync(false);
 
-            // Act
             var result = await _controller.Update(1, product);
 
             // Assert
@@ -149,10 +133,8 @@ namespace ProductAPI.Test
         [Fact]
         public async Task Delete_ReturnsOkResult_WhenProductDeleted()
         {
-            // Arrange
             _mockService.Setup(s => s.DeleteAsync(1)).ReturnsAsync(true);
 
-            // Act
             var result = await _controller.Delete(1);
 
             // Assert
@@ -163,10 +145,8 @@ namespace ProductAPI.Test
         [Fact]
         public async Task Delete_ReturnsNotFound_WhenProductNotFound()
         {
-            // Arrange
             _mockService.Setup(s => s.DeleteAsync(1)).ReturnsAsync(false);
 
-            // Act
             var result = await _controller.Delete(1);
 
             // Assert
