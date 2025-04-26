569f8c9c720101ee497686c8a2ceb78c376db2da
Aung Myint Myat <61227624+AungMyintMyat-30@users.noreply.github.com>
2025-04-26 19:03:59 +0630

    Create TokenBuilder.cs

---
+++ ProductInfrastructure/Services/TokenBuilder.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using ProductCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInfrastructure.Services
+{
+    public class TokenBuilder(IConfiguration configuration, ILogger<TokenBuilder> logger) : ITokenBuilder
+    {
+        private readonly IConfiguration _configuration = configuration;
+        private readonly ILogger<TokenBuilder> _logger = logger;
+
+        /// <summary>
+        /// Generate Access Token
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public string GenerateAccessToken(string username)
+        {
+            var jwtKey = _configuration["Jwt:Key"] ?? "";
+            _logger.LogInformation($"JWT Generation Key: {jwtKey}");
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.NameIdentifier, username),
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30), // Token expiration time
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
