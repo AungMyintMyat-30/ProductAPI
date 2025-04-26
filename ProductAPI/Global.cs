5d8e01004c4f7420cec6e25ef36b0edb0fec76ab
Aung Myint Myat <aungmmyat30599@gmail.com>
2025-04-26 20:20:33 +0630

    Create Global.cs

---
+++ ProductAPI/Global.cs
@@ -0,0 +1,17 @@
+﻿global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.OpenApi.Models;
+global using ProductAPI.Interfaces;
+global using ProductAPI.Middleware;
+global using ProductAPI.Services;
+global using ProductCore.Interfaces;
+global using ProductInfrastructure.Data;
+global using ProductInfrastructure.Services;
+global using Serilog;
+global using Serilog.Events;
+global using System.Reflection;
+global using System.Text;
+internal class Global
+{
+}
