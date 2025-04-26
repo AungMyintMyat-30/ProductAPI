418037a296a2c580b19a8425d9000226066029c5
Aung Myint Myat <aungmmyat30599@gmail.com>
2025-04-26 20:20:36 +0630

    Create GlobalExceptionMiddleware.cs

---
+++ ProductAPI/Middleware/GlobalExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using ProductCore.Models;
+using System.Net;
+
+namespace ProductAPI.Middleware
+{
+    public class GlobalExceptionMiddleware(RequestDelegate next, ILogger<GlobalExceptionMiddleware> logger)
+    {
+        private readonly RequestDelegate _next = next;
+        private readonly ILogger<GlobalExceptionMiddleware> _logger = logger;
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Runtime Error: {message}", ex.Message);
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            DefaultResponseModel model = new()
+            {
+                Success = false,
+                Meta = null,
+                Code = StatusCodes.Status500InternalServerError,
+                Data = "",
+                Error = ex.Message
+            };
+
+            return context.Response.WriteAsync(JsonConvert.SerializeObject(model));
+        }
+    }
+}
