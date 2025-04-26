dddae62cb759cfb42d4a41ba140368e225ec49f9
Aung Myint Myat <61227624+AungMyintMyat-30@users.noreply.github.com>
2025-04-26 19:03:52 +0630

    Create ITokenBuilder.cs

---
+++ ProductCore/Interfaces/ITokenBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCore.Interfaces
+{
+    public interface ITokenBuilder
+    {
+        public string GenerateAccessToken(string user);
+    }
+}
